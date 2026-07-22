4703862da1b1d543cab342ec2827fe4e2cc6eaf8
Ambati Nikhil Datta <157356667+NIKHIL0-0@users.noreply.github.com>
2026-07-22 20:06:23 +0530

    Add: convert-bst-to-greater-tree - submission-3

---
+++ Data Structures & Algorithms/convert-bst-to-greater-tree/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        total = 0
+        sys.setrecursionlimit(2000)
+
+        def dfs(node):
+            nonlocal total
+
+            if not node:
+                return
+
+            dfs(node.right)
+
+            total += node.val
+            node.val = total
+
+            dfs(node.left)
+
+        dfs(root)
+        return root
